dec7abd34212f7ccec8e3682fa8793c221fd29f2
cenz19 <86843417+cenz19@users.noreply.github.com>
2023-12-13 13:44:46 +0700

    UPDATE

--- app/src/main/java/com/nmpubaya/cerbung/HomeFragment.kt
+++ app/src/main/java/com/nmpubaya/cerbung/HomeFragment.kt
@@ -1,58 +1,79 @@
 package com.nmpubaya.cerbung
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.toolbox.StringRequest
+import com.nmpubaya.cerbung.databinding.FragmentHomeBinding
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import com.android.volley.Request
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    var playlists:ArrayList<Cerbung> = arrayListOf()
+    private lateinit var binding:FragmentHomeBinding
+
+    fun updateList() {
+        //Fungsi ini untuk setup adapter
+        val lm = LinearLayoutManager(activity)
+        with(binding.playListRecView) {
+            layoutManager = lm
+            setHasFixedSize(true)
+            adapter = PlaylistAdapter(playlists)
         }
     }
-
+    fun reload(){
+        val q = Volley.newRequestQueue(activity)
+        val url = "https://ubaya.me/native/160421072/get_playlist.php"
+        //val url = "http://10.0.2.2/music_zd/get_playlist.php"
+        var stringRequest = StringRequest(
+            Request.Method.POST, url,
+            {
+                Log.d("apiresult", it.toString())
+                val obj = JSONObject(it)
+                if(obj.getString("result") == "OK"){
+                    val data = obj.getJSONArray("data")
+                    val sType = object : TypeToken<List<Cerbung>>() { }.type
+                    playlists = Gson().fromJson(data.toString(), sType) as ArrayList<Cerbung>
+                    Log.d("apiresult", playlists.toString())
+                    updateList()
+                }
+            },
+            {
+                Log.e("apiresult", it.message.toString())
+            })
+        q.add(stringRequest)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        reload()
+    }
+    override fun onResume() {
+        super.onResume()
+        reload()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
