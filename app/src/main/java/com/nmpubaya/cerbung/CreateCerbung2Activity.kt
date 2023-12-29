6058b183d27b238278fb349ab546da5215a4416e
abayup14 <abayup1498@gmail.com>
2023-12-29 17:05:41 +0700

    Tambah Binding di Activity2 dan Activity3

--- app/src/main/java/com/nmpubaya/cerbung/CreateCerbung2Activity.kt
+++ app/src/main/java/com/nmpubaya/cerbung/CreateCerbung2Activity.kt
@@ -2,10 +2,13 @@ package com.nmpubaya.cerbung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nmpubaya.cerbung.databinding.ActivityCreateCerbung2Binding
 
 class CreateCerbung2Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateCerbung2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_cerbung2)
+        binding = ActivityCreateCerbung2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/nmpubaya/cerbung/CreateCerbung3Activity.kt
+++ app/src/main/java/com/nmpubaya/cerbung/CreateCerbung3Activity.kt
@@ -2,10 +2,13 @@ package com.nmpubaya.cerbung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nmpubaya.cerbung.databinding.ActivityCreateCerbung3Binding
 
 class CreateCerbung3Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateCerbung3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_cerbung3)
+        binding = ActivityCreateCerbung3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
