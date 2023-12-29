5ef834e929093d28e9d74f790693783c056004e9
cenz19 <86843417+cenz19@users.noreply.github.com>
2023-12-29 16:33:26 +0700

    Create cerbung 1 2 3 added

---
+++ app/src/main/java/com/nmpubaya/cerbung/CreateCerbung2Activity.kt
@@ -0,0 +1,11 @@
+package com.nmpubaya.cerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateCerbung2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_cerbung2)
+    }
+}
---
+++ app/src/main/java/com/nmpubaya/cerbung/CreateCerbung3Activity.kt
@@ -0,0 +1,11 @@
+package com.nmpubaya.cerbung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateCerbung3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_cerbung3)
+    }
+}
