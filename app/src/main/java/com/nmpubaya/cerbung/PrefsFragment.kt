f3b2f2c3570365b9a36bb6039d9ca32a6b73536a
abayup14 <abayup1498@gmail.com>
2023-12-31 05:03:18 +0700

    Create Dark Mode

--- app/src/main/java/com/nmpubaya/cerbung/PrefsFragment.kt
+++ app/src/main/java/com/nmpubaya/cerbung/PrefsFragment.kt
@@ -1,32 +1,20 @@
 package com.nmpubaya.cerbung
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import com.nmpubaya.cerbung.databinding.FragmentPrefsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PrefsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrefsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentPrefsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,25 +22,44 @@ class PrefsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prefs, container, false)
+        // return inflater.inflate(R.layout.fragment_prefs, container, false)
+        binding = FragmentPrefsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedFile = "com.nmpubaya.cerbung"
+        val sharedPreferences = activity?.getSharedPreferences(sharedFile, Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+        val nightMode = sharedPreferences?.getBoolean("night", false)
+
+        if (nightMode == true) {
+            binding.switchDarkMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        binding.switchDarkMode.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor?.putBoolean("night", false)
+                editor?.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor?.putBoolean("night", true)
+                editor?.apply()
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrefsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PrefsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
