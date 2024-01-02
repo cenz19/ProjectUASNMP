58900d834a408669dd2cf86c9f6800295253c14e
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-01-02 12:23:25 +0700

    Update Paragraph.kt

--- app/src/main/java/com/nmpubaya/cerbung/Paragraph.kt
+++ app/src/main/java/com/nmpubaya/cerbung/Paragraph.kt
@@ -1,9 +1,12 @@
 package com.nmpubaya.cerbung
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.Date
 
+@Parcelize
 data class Paragraph(var id:Int,
                      var isi:String,
                      var waktu_buat:Date,
                      var cerbung_id:Int,
-                     var author_id:Int)
+                     var author_id:Int) : Parcelable
