a3a7257302e13823dc821c2809f2079679aa36a8
cenz19 <86843417+cenz19@users.noreply.github.com>
2024-01-04 14:13:07 +0700

    SEEGGGGSSSS

--- app/src/main/java/com/nmpubaya/cerbung/ReadPublicFragment.kt
+++ app/src/main/java/com/nmpubaya/cerbung/ReadPublicFragment.kt
@@ -1,5 +1,4 @@
 package com.nmpubaya.cerbung
-
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
@@ -25,17 +24,101 @@ import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-
 private const val ARG_CERBUNG = "cerbung"
 private const val ARG_ID = "id"
-
-
 
 class ReadPublicFragment : Fragment() {
     private var cerbung: Cerbung? = null
     var paragraphs: ArrayList<Paragraph> = arrayListOf()
     private lateinit var binding: FragmentReadPublicBinding
     private var users_id: Int? =null
+    companion object {
+        @JvmStatic
+        fun newInstance(cerbung: Cerbung, users_id: Int) =
+            ReadPublicFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_CERBUNG, cerbung)
+                    putInt(ARG_ID, users_id)
+                }
+            }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            cerbung = it.getParcelable(ARG_CERBUNG)
+            users_id = it.getInt(ARG_ID)
+        }
+
+//        binding.txtTesting.text = "testing"
+
+//        binding.txtTesting.text = "Ini cerbung id: "+cerbung!!.id+ " dan ini user id: "+ users_id!!
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_read_public, container, false)
+        binding = FragmentReadPublicBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = "Read Cerbung"
+        val url = cerbung?.url_gambar
+        val builder = Picasso.Builder(view.context)
+        builder.listener { picasso, uri, exception ->  exception.printStackTrace()}
+        Picasso.get().load(url).into(binding.imgCerbung)
+        binding.txtJudul.text = cerbung?.title
+        binding.txtListCount.text = cerbung?.num_paragraph.toString()
+        binding.txtLikeCount.text = cerbung?.num_likes.toString()
+        binding.txtCreator.text = cerbung?.username
+        binding.txtDateCreated.text = cerbung?.waktu_dibuat
+        binding.txtGenre.text = cerbung?.genre
+
+
+        binding.btnFollow.setOnClickListener {
+            val cerbung_id = cerbung?.id
+            val user_id = users_id
+//            Toast.makeText(activity, "Cer ID: $cerbung_id, User: $user_id", Toast.LENGTH_LONG).show()
+            followCerbung(cerbung_id!!, user_id!!)
+        }
+
+        var isi = binding.txtNewPar.text.toString()
+        val date = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("yyyy-MM-dd")
+        val waktu_buat = formatter.format(date)
+        val user_id = users_id!!
+
+        binding.txtTesting.text = String.format("waktu $waktu_buat, cerbungid ${cerbung!!.id} , userid $user_id")
+        binding.btnSubmit.setOnClickListener {
+            val q = Volley.newRequestQueue(activity)
+            val url = "https://ubaya.me/native/160421005/create_paragraph.php"
+            val stringRequest = object : StringRequest(com.android.volley.Request.Method.POST, url, {
+                Toast.makeText(activity, "Added Successfully", Toast.LENGTH_LONG).show()
+
+            },{
+
+            }){
+                override fun getParams(): MutableMap<String, String>? {
+                    val params = HashMap<String, String>()
+                    params["isi"] = binding.txtNewPar.text.toString()
+                    params["waktu_buat"] = waktu_buat.toString()
+                    params["cerbung_id"] = cerbung?.id.toString()
+                    params["users_id"] = users_id.toString()
+                    return params
+                }
+
+            }
+            q.add(stringRequest)
+        }
+
+        binding.txtNewPar.doOnTextChanged { text, start, before, count ->
+            binding.txtChar.text = binding.txtNewPar.text?.count().toString()
+        }
+    }
 
     fun updateParagraph() {
         val lm = LinearLayoutManager(activity)
@@ -48,7 +131,7 @@ class ReadPublicFragment : Fragment() {
     fun refresh() {
         val q = Volley.newRequestQueue(activity)
         val url = "https://ubaya.me/native/160421005/get_all_paragraphs.php"
-        var stringRequest = object: StringRequest(Request.Method.POST, url,
+        var stringRequest = object: StringRequest(com.android.volley.Request.Method.POST, url,
             {
                 Log.d("apiresult", it.toString())
                 val obj = JSONObject(it)
@@ -71,96 +154,67 @@ class ReadPublicFragment : Fragment() {
         q.add(stringRequest)
     }
 
-    fun addParagraph(isi:String, waktu_buat:String, cerbung_id: Int, users_id: Int) {
-        val q = Volley.newRequestQueue(activity)
-        val url = "https://ubaya.me/native/160421005/create_paragraph.php"
-        val dialog = androidx.appcompat.app.AlertDialog.Builder(requireContext())
-        val stringRequest = object : StringRequest(Request.Method.POST, url,
-            {
-                Log.d("apiresult", it)
-                val obj = JSONObject(it)
-                if (obj.getString("result") == "OK") {
-                    val msg = obj.getString("msg")
-                    val sType = object : TypeToken<Int>() { }.type
-                    val id_cerbung = Gson().fromJson(msg.toString(), sType) as Int
-                    val strReq = object : StringRequest(Request.Method.POST, url,
-                        {
-                            Log.d("apiresult", it)
-                            val objt = JSONObject(it)
-                            if (objt.getString("result") == "OK") {
-                                dialog.setMessage("Successfully created new paragraph.\nRefresh to see it.")
-                                dialog.create().show()
-                            }
-                        },
-                        {
-                            Log.e("apiresult", it.message.toString())
-                            dialog.setMessage("Failed to create paragraph")
-                            dialog.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
-                                dialog.dismiss()
-                            })
-                            dialog.create().show()
-                        }
-                    ) {
-                        override fun getParams(): MutableMap<String, String>? {
-                            val params = HashMap<String, String>()
-                            params["isi"] = isi
-                            params["waktu_buat"] = waktu_buat.toString()
-                            params["cerbung_id"] = id_cerbung.toString()
-                            params["users_id"] = users_id.toString()
-                            return params
-                        }
-                    }
-                    q.add(strReq)
-                } else {
-                    dialog.setMessage("Failed to create paragraph")
-                    dialog.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
-                        dialog.dismiss()
-                    })
-                    dialog.create().show()
-                }
-            },
-            {
-                Log.e("apiresult", it.printStackTrace().toString())
-            }
-        ) {
-            override fun getParams(): MutableMap<String, String>? {
-                val params = HashMap<String, String>()
-                params["cerbung_id"] = cerbung_id.toString()
-                params["users_id"] = users_id.toString()
-                return params
-            }
-        }
-        q.add(stringRequest)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            cerbung = it.getParcelable(ARG_CERBUNG)
-            users_id = it.getInt(ARG_ID)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        refresh()
-        arguments?.let {
-            cerbung = it.getParcelable(ARG_CERBUNG)
-            users_id = it.getInt(ARG_ID)
-        }
-        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
-        (activity as AppCompatActivity).supportActionBar?.title = "Read Cerbung"
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_read_public, container, false)
-        binding = FragmentReadPublicBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+//    fun addParagraph(isi:String, waktu_buat:String, cerbung_id: Int, users_id: Int) {
+//        val q = Volley.newRequestQueue(activity)
+//        val url = "https://ubaya.me/native/160421005/create_paragraph.php"
+//        val dialog = androidx.appcompat.app.AlertDialog.Builder(requireContext())
+//        val stringRequest = object : StringRequest(Request.Method.POST, url,
+//            {
+//                Log.d("apiresult", it)
+//                val obj = JSONObject(it)
+//                if (obj.getString("result") == "OK") {
+//                    val msg = obj.getString("msg")
+//                    val sType = object : TypeToken<Int>() { }.type
+//                    val id_cerbung = Gson().fromJson(msg.toString(), sType) as Int
+//                    val strReq = object : StringRequest(Request.Method.POST, url,
+//                        {
+//                            Log.d("apiresult", it)
+//                            val objt = JSONObject(it)
+//                            if (objt.getString("result") == "OK") {
+//                                dialog.setMessage("Successfully created new paragraph.\nRefresh to see it.")
+//                                dialog.create().show()
+//                            }
+//                        },
+//                        {
+//                            Log.e("apiresult", it.message.toString())
+//                            dialog.setMessage("Failed to create paragraph")
+//                            dialog.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+//                                dialog.dismiss()
+//                            })
+//                            dialog.create().show()
+//                        }
+//                    ) {
+//                        override fun getParams(): MutableMap<String, String>? {
+//                            val params = HashMap<String, String>()
+//                            params["isi"] = isi
+//                            params["waktu_buat"] = waktu_buat.toString()
+//                            params["cerbung_id"] = id_cerbung.toString()
+//                            params["users_id"] = users_id.toString()
+//                            return params
+//                        }
+//                    }
+//                    q.add(strReq)
+//                } else {
+//                    dialog.setMessage("Failed to create paragraph")
+//                    dialog.setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+//                        dialog.dismiss()
+//                    })
+//                    dialog.create().show()
+//                }
+//            },
+//            {
+//                Log.e("apiresult", it.printStackTrace().toString())
+//            }
+//        ) {
+//            override fun getParams(): MutableMap<String, String>? {
+//                val params = HashMap<String, String>()
+//                params["cerbung_id"] = cerbung_id.toString()
+//                params["users_id"] = users_id.toString()
+//                return params
+//            }
+//        }
+//        q.add(stringRequest)
+//    }
 
     fun followCerbung(cerbung_id: Int, users_id: Int) {
         val q = Volley.newRequestQueue(activity)
@@ -205,56 +259,18 @@ class ReadPublicFragment : Fragment() {
         }
         q.add(stringRequest)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
+        refresh()
+        arguments?.let {
+            cerbung = it.getParcelable(ARG_CERBUNG)
+            users_id = it.getInt(ARG_ID)
+        }
         (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
         (activity as AppCompatActivity).supportActionBar?.title = "Read Cerbung"
-        val url = cerbung?.url_gambar
-        val builder = Picasso.Builder(view.context)
-        builder.listener { picasso, uri, exception ->  exception.printStackTrace()}
-        Picasso.get().load(url).into(binding.imgCerbung)
-        binding.txtJudul.text = cerbung?.title
-        binding.txtListCount.text = cerbung?.num_paragraph.toString()
-        binding.txtLikeCount.text = cerbung?.num_likes.toString()
-        binding.txtCreator.text = cerbung?.username
-        binding.txtDateCreated.text = cerbung?.waktu_dibuat
-        binding.txtGenre.text = cerbung?.genre
-
-        binding.btnFollow.setOnClickListener {
-            val cerbung_id = cerbung?.id
-            val user_id = users_id
-//            Toast.makeText(activity, "Cer ID: $cerbung_id, User: $user_id", Toast.LENGTH_LONG).show()
-            followCerbung(cerbung_id!!, user_id!!)
-        }
-
-        var isi = binding.txtNewPar.text.toString()
-        val date = Calendar.getInstance().time
-        val formatter = SimpleDateFormat("yyyy-MM-dd")
-        val waktu_buat = formatter.format(date)
-        val user_id = users_id!!
-
-        binding.btnSubmit.setOnClickListener {
-            if (isi != null) {
-                addParagraph(isi, waktu_buat, cerbung!!.id, user_id)
-            }
-        }
-
-        binding.txtNewPar.doOnTextChanged { text, start, before, count ->
-            binding.txtChar.text = binding.txtNewPar.text?.count().toString()
-        }
     }
 
 
 
-    companion object {
-        @JvmStatic
-        fun newInstance(cerbung: Cerbung, users_id: Int) =
-            ReadPublicFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable(ARG_CERBUNG, cerbung)
-                    putInt(ARG_ID, users_id)
-                }
-            }
-    }
+
 }
